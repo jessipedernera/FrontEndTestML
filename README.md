f50254451e2bbbcf57d5eea167c1f032364e8fab
Pedernera <jessica.pedernera@intel.com>
2016-09-12 20:09:08 -0300

    adding carousel and tabs

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# FrontEndTestML
+# FrontEnd Test - Mercado Libre
+
+> This repository includes a possible implementation of a widget, developed
+> using the stack of HTML5 technologies based on a prototype.
+
+### Development
+
+
+### Usage
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,5 @@
 var dropdown1 = new ch.Dropdown(document.querySelector('.demo-dropdown1'));
 var dropdown2 = new ch.Dropdown(document.querySelector('.demo-dropdown2'));
+var carousel = new ch.Carousel(ch('.demo-carousel')[0], {
+    autoMargin: false
+}); 
